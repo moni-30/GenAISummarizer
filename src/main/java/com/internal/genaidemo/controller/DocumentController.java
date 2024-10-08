6052a405a8189a61f25f1a24d5f96ecbb29c0b43
moni-30 <76830990+moni-30@users.noreply.github.com>
2024-10-09 00:36:14 +0530

    Update DocumentController.java

--- src/main/java/com/internal/genaidemo/controller/DocumentController.java
+++ src/main/java/com/internal/genaidemo/controller/DocumentController.java
@@ -1,24 +1,32 @@
 package com.internal.genaidemo.controller;
 
-import com.internal.genaidemo.service.DocumentSummarizationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.internal.genaidemo.service.DocumentSummarizationService;
+import com.internal.genaidemo.service.DocumentSummaryCustomiseService;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api/documents")
 public class DocumentController {
 
-
     @Autowired
     private DocumentSummarizationService documentSummarizationService;
+    
+    @Autowired
+    private DocumentSummaryCustomiseService summaryService;
 
     @PostMapping("/summarize")
     public ResponseEntity<String> summarizeDocument(@RequestParam("file") MultipartFile file) {
         try {
-            String summary = documentSummarizationService.summarizeDocument1(file);
+            String summary = summaryService.summarizeDocument(file);
             return ResponseEntity.ok(summary);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Failed to process document: " + e.getMessage());
